185dcf65d34a104cac8ed1a516cf95df39e4d4f1
flagtrader16 <flagtrader16@gmail.com>
2026-02-01 14:54:40 +0100

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,12 @@
 # telegram_bot.py
 import requests
+import pandas as pd
 
 def send_image(
     image_path: str,
     caption: str,
     bot_token: str,
-    chat_id: str
+    chat_id: str,
 ):
     url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
 
@@ -20,3 +21,16 @@ def send_image(
     if r.status_code != 200:
         raise RuntimeError(f"Telegram error: {r.text}")
 
+def send_file(file_path, caption, bot_token, chat_id):
+    url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
+    with open(file_path, "rb") as f:
+        files = {"document": f}
+        data = {
+            "chat_id": chat_id,
+            "caption": caption
+        }
+        r = requests.post(url, data=data, files=files)
+
+    if r.status_code != 200:
+        raise RuntimeError(r.text)
+        
